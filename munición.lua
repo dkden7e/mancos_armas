0cd9761ed38e98a588c3aad154f48b36df28e71a
Benito "DK_Den7e" L <10780603+dkden7e@users.noreply.github.com>
2022-03-22 07:21:36 +0000

    Update munición.lua

--- munición.lua
+++ munición.lua
@@ -1,81 +1,109 @@
 Ammo = {
 	['ammo-22'] = {
 		label = '.22 Long Rifle',
+		description = 'Básicamente, una bala de juguete que viaja a velocidades de una bala de rifle. Alta precisión, baja letalidad.',
 		weight = 3,
 	},
 
 	['ammo-38'] = {
 		label = '.38 Long Colt',
+		description = 'Calibre histórico prácticamente en desuso, algunos revólveres de antiguo diseño todavía lo usan.',
 		weight = 10,
 	},
 
 	['ammo-44'] = {
 		label = '.44 Magnum',
+		description = 'Uno de dos calibres conocidos como "destrozamuñecas", gran calibre de pistola diseñado originalmene para cazar osos y renos, de poca utilidad práctica a día de hoy.',
 		weight = 16,
 	},
 
 	['ammo-45'] = {
 		label = '.45 ACP',
+		description = 'Uno de los dos calibres de pistola más comunes y prácticos a día de hoy. Bala más grande pero más lenta que 9mm Parabellum.',
 		weight = 9,
 	},
 
 	['ammo-50'] = {
 		label = '.50 AE',
+		description = 'Uno de dos calibres conocidos como "destrozamuñecas", gran calibre de pistola diseñado originalmene para compensar "otras carencias" de tamaño de los diseñadores, de poca utilidad práctica a día de hoy.',
 		weight = 19,
 	},
 
 	['ammo-9'] = {
-		label = '9mm',
+		label = '9mm Parabellum',
+		description = 'Uno de los dos calibres de pistola más comunes y prácticos a día de hoy. Bala más rápida pero más pequeña que .45 ACP.',
 		weight = 8,
 	},
 
 	['ammo-flare'] = {
-		label = 'Flare',
-		weight = 38,
+		label = 'Bengala',
+		description = 'Una bengala de uso manual, ¿qué necesitas que te explique?.',
+		weight = 100,
 	},
 
 	['ammo-heavysniper'] = {
 		label = '.50 BMG',
-		weight = 45,
+		description = 'Bala de gran calibre y alta velocidad utilizada por ametralladoras pesadas y rifles de francotirador/antimaterial.',
+		weight = 80,
 	},
 
 	['ammo-musket'] = {
-		label = 'Musket ammo',
-		weight = 30,
+		label = 'Cargas de mosquete',
+		description = 'Una canica de metal pulido junto a una bolsita individual de pólvora negra.',
+		weight = 50,
 	},
 
 	['ammo-rifle'] = {
-		label = '5.56',
-		weight = 4,
+		label = '5.56 NATO',
+		description = 'El calibre por excelencia de los rifles de asalto modernos occidentales. Alta velocidad y precisión con bajo peso.',
+		weight = 15,
 	},
 
 	['ammo-rifle2'] = {
-		label = '7.62',
-		weight = 8,
+		label = '7.62x54mmR',
+		description = 'El calibre por excelencia de los rifles de asalto modernos (ex)soviéticos. Alta velocidad y precisión con bajo peso.',
+		weight = 15,
+	},
+
+	['ammo-rifle3'] = {
+		label = '7.62 NATO',
+		description = 'Versátil calibre multipropósito, de mayor tamaño pero menor velocidad que 7.62x54mmR y 5.56 NATO, cuenta con mayor alcance e impacto y es utilizado por rifles de asalto pesados, ametralladoras medias y rifles de precisión.',
+		weight = 20,
 	},
 
 	['ammo-shotgun'] = {
-		label = '12 Gauge',
+		label = 'Calibre 12',
+		description = 'El calibre de escopeta más común, capaz de hacer grandes destrozos.',
 		weight = 38,
 	},
 
 	['ammo-sniper'] = {
-		label = '7.62 NATO',
-		weight = 11,
+		label = '.338 Lapua Magnum',
+		description = 'Versátil calibre semipesado utilizado por rifles de francotirador. Más ligero que .50BMG pero con el mismo alcance y un impacto mucho mayor que 7.62 NATO.',
+		weight = 30,
 	},
 	
-        ['ammo-rocket'] = {
-		label = 'Rocket',
-		weight = 20,
+	['ammo-rocket'] = {
+		label = 'Cohete RPG',
+		description = 'Cohete no guiado con una cabeza de guerra HEDP (alto explosivo doble propósito), capaz de penetrar blindajes y causar daños personales con su metralla.',
+		weight = 1000,
+	},
+	
+	['ammo-missile'] = {
+		label = 'Misil infrarrojo',
+		description = 'Cohete con sistema de guía infrarrojo capaz de fijar objetivos aéreos y terrestres y cabeza de guerra HEDP.',
+		weight = 1500,
 	},
 
-        ['ammo-40mm'] = {
-		label = '40MM Granade',
-		weight = 17,
+	['ammo-40mm'] = {
+		label = 'Proyectil 40mm (HE)',
+		description = 'Granada alto explosivo preparada para ser lanzada por un lanzagranadas.',
+		weight = 150,
 	},
 
 	['ammo-40mm2'] = {
-		label = '40MM Smoke',
-		weight = 16,
-        },
+		label = 'Proyectil 40mm (fumígeno)',
+		description = 'Granada de humo preparada para ser lanzada por un lanzagranadas.',
+		weight = 150,
+		},
 }
